0a416d0b9ed1369433ed978a48a48e835dd77ac5
parkeunsik <qkrdmstlr3@naver.com>
2022-05-03 01:23:04 +0900

    [test] implement tabbs api test code

---
+++ src/chrome/__test__/tabs.test.ts
@@ -0,0 +1,13 @@
+import { getAllAudibleTab } from '../tabs';
+
+const tabsQueryMock = chrome.tabs.query as jest.MockedFunction<typeof chrome.tabs.query>;
+
+describe('Tabs test', () => {
+  it('getAllAudibleTab test', async () => {
+    tabsQueryMock.mockReturnValue(new Promise((res) => res([])));
+    const result = await getAllAudibleTab();
+
+    expect(result).toHaveLength(0);
+    expect(tabsQueryMock).toBeCalledTimes(1);
+  });
+});
