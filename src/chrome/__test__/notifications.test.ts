d19de863d4362a5dbe98e14dd3d37e42b5c09aff
parkeunsik <qkrdmstlr3@naver.com>
2022-05-03 00:08:50 +0900

    [test] implement notifications api test code

---
+++ src/chrome/__test__/notifications.test.ts
@@ -0,0 +1,28 @@
+import { warn, create } from '../notifications';
+
+const createMock = chrome.notifications.create as jest.MockedFunction<typeof chrome.notifications.create>;
+
+beforeEach(() => {
+  createMock.mockClear();
+});
+
+describe('Notification test', () => {
+  it('create test', async () => {
+    const dummyObj = {
+      type: 'basic' as chrome.notifications.TemplateType,
+      iconUrl: '/',
+      title: 'watch out',
+      message: 'message',
+    };
+    create(dummyObj);
+
+    expect(createMock).toBeCalledTimes(1);
+    expect(createMock).toBeCalledWith(dummyObj);
+  });
+
+  it('warn test', () => {
+    warn('message');
+
+    expect(createMock).toBeCalledTimes(1);
+  });
+});
