a7c6eee24f4728985cc921d7d8030f241cf699da
parkeunsik <qkrdmstlr3@naver.com>
2022-05-18 02:38:11 +0900

    [refactor] adjust times
    - as a paper, after listening 1 hour then take a rest at 10 minute is the best

--- src/Background/index.ts
+++ src/Background/index.ts
@@ -5,12 +5,13 @@ class Background {
   stopTimer: Timer;
   alarmTimer: Timer;
   COUNT_MINUTE_UNIT: number = 1;
-  LIMITED_MINUTE_UNIT: number = 30;
-  INITIALIZE_TIMER_STANDARD_MINUTE: number = 1;
+  LIMITED_MINUTE_UNIT: number = 60;
+  INITIALIZE_TIMER_STANDARD_MINUTE: number = 10;
 
   constructor() {
     this.stopTimer = new Timer('stopTimer');
     this.alarmTimer = new Timer('alarmTimer');
+    chrome.storage.set('checked', true);
     chrome.alarms.createAlarm(this.COUNT_MINUTE_UNIT);
     chrome.alarms.onAlarm.addListener(this.addAlarm.bind(this));
   }
@@ -20,14 +21,13 @@ class Background {
     const alarmTimerMinutes = await this.alarmTimer.getMinute();
     const stopTimerMinutes = await this.stopTimer.getMinute();
 
-    //FIXME: For test alarmTimer minute
     chrome.action.setBadgeText({
       text: `${alarmTimerMinutes}`,
     });
 
     const isAlarm = alarmTimerMinutes && !(alarmTimerMinutes % this.LIMITED_MINUTE_UNIT);
     if (audibleTabs.length) {
-      if (isAlarm) chrome.notifications.warn(`음악을 재생한지 ${alarmTimerMinutes}분이 지났어요!`);
+      if (isAlarm) chrome.notifications.warn(`소리를 재생한지 ${alarmTimerMinutes}분이 지났어요!`);
       this.alarmTimer.increaseMinute();
       this.stopTimer.initializeMinute();
     }
