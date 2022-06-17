2c27711d394799d73c0d04952cacbf7dbcc6afcb
parkeunsik <qkrdmstlr3@naver.com>
2022-06-17 19:27:19 +0900

    [refactor] change warning text
    - change from "조심하세요" to "휴식을 취하세요!"

--- src/chrome/notifications.ts
+++ src/chrome/notifications.ts
@@ -4,7 +4,7 @@ export const warn = (message: string) => {
   chrome.notifications.create({
     type: 'basic',
     iconUrl: Warning,
-    title: '조심하세요!',
+    title: '휴식을 취하세요!',
     message,
   });
 };
